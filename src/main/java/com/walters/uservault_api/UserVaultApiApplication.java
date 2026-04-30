4a1640b4e1d842f96ddd46db7d546b1f40ea181d
William Walters <wwalters357@gmail.com>
2026-04-29 21:21:45 -0400

    Create new UserVault project to allow user to create and delete stored users.

---
+++ src/main/java/com/walters/uservault_api/UserVaultApiApplication.java
@@ -0,0 +1,13 @@
+package com.walters.uservault_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserVaultApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserVaultApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walters/uservault_api/UserVaultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walters.uservault_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserVaultApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
